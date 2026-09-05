fcc7ce64f91adae4659657edc72a6219b7421eb6
wheexed <133235593+wheexed@users.noreply.github.com>
2026-09-05 12:43:07 +0300

    Create 2.0finelpong.c

---
+++ src/2.0finelpong.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+
+#define WIDTH 80
+#define HEIGHT 25
+#define WIN_SCORE 21
+
+int ball_x;
+int ball_y;
+int ball_dx;
+int ball_dy;
+
+int left_y;
+int right_y;
+
+int left_score;
+int right_score;
+
+char command;
+
+void draw(void) {
+  int y;
+  int x;
+
+  printf("\033[H\033[J");
+  printf("Player 1: %d                              Player 2: %d\n",
+         left_score, right_score);
+
+  for (y = 0; y < HEIGHT; y++) {
+    for (x = 0; x < WIDTH; x++) {
+      if (y == 0 || y == HEIGHT - 1) {
+        printf("-");
+      } else if (x == 0 || x == WIDTH - 1) {
+        printf("|");
+      } else if (x == ball_x && y == ball_y) {
+        printf("O");
+      } else if (x == WIDTH / 2 && y % 2 == 0) {
+        printf("|");
+      } else if (x == 2 && y >= left_y - 1 && y <= left_y + 1) {
+        printf("|");
+      } else if (x == WIDTH - 3 && y >= right_y - 1 &&
+                 y <= right_y + 1) {
+        printf("|");
+      } else {
+        printf(" ");
+      }
+    }
+
+    printf("\n");
+  }
+}
+
+char get_command(void) {
+  char c;
+
+  while (1) {
+    c = getchar();
+
+    if (c == 'A' || c == 'a' ||
+        c == 'Z' || c == 'z' ||
+        c == 'K' || c == 'k' ||
+        c == 'M' || c == 'm' ||
+        c == ' ') {
+      return c;
+    }
+  }
+}
+
+void move_rackets(void) {
+  if ((command == 'A' || command == 'a') && left_y > 2) {
+    left_y--;
+  }
+
+  if ((command == 'Z' || command == 'z') && left_y < HEIGHT - 3) {
+    left_y++;
+  }
+
+  if ((command == 'K' || command == 'k') && right_y > 2) {
+    right_y--;
+  }
+
+  if ((command == 'M' || command == 'm') && right_y < HEIGHT - 3) {
+    right_y++;
+  }
+}
+
+void move_ball(void) {
+  ball_x += ball_dx;
+  ball_y += ball_dy;
+
+  if (ball_y <= 1) {
+    ball_y = 1;
+    ball_dy = 1;
+  }
+
+  if (ball_y >= HEIGHT - 2) {
+    ball_y = HEIGHT - 2;
+    ball_dy = -1;
+  }
+
+  if (ball_x == 2 && ball_dx < 0 &&
+      ball_y >= left_y - 1 && ball_y <= left_y + 1) {
+    ball_dx = 1;
+  }
+
+  if (ball_x == WIDTH - 3 && ball_dx > 0 &&
+      ball_y >= right_y - 1 && ball_y <= right_y + 1) {
+    ball_dx = -1;
+  }
+}
+
+void check_goal(void) {
+  if (ball_x <= 0) {
+    right_score++;
+    ball_x = WIDTH / 2;
+    ball_y = HEIGHT / 2;
+    ball_dx = 1;
+  }
+
+  if (ball_x >= WIDTH - 1) {
+    left_score++;
+    ball_x = WIDTH / 2;
+    ball_y = HEIGHT / 2;
+    ball_dx = -1;
+  }
+}
+
+int main(void) {
+  ball_x = WIDTH / 2;
+  ball_y = HEIGHT / 2;
+  ball_dx = 1;
+  ball_dy = 1;
+
+  left_y = HEIGHT / 2;
+  right_y = HEIGHT / 2;
+
+  left_score = 0;
+  right_score = 0;
+
+  draw();
+
+  while (left_score < WIN_SCORE && right_score < WIN_SCORE) {
+    command = get_command();
+
+    move_rackets();
+    move_ball();
+    check_goal();
+
+    draw();
+  }
+
+  if (left_score == WIN_SCORE) {
+    printf("Player 1 wins!\n");
+  } else {
+    printf("Player 2 wins!\n");
+  }
+
+  return 0;
+}
